03024b1393c5d2d3124988da5d751e69f4f424f9
Oleg <koe@ipcpi.ru>
2019-12-06 18:02:09 +0700

    Писал на работе

--- task4/server/src/main/java/ru/focusstart/ServerMain.java
+++ task4/server/src/main/java/ru/focusstart/ServerMain.java
@@ -2,12 +2,10 @@ package ru.focusstart;
 
 import ru.focusstart.model.ServerModel;
 
-import java.io.IOException;
-
 public class ServerMain {
     public static void main(String[] args) {
         ServerModel chatServer = ServerModel.getInstance();
         chatServer.start();
-        chatServer.stop();
+        chatServer.watch();
     }
 }
--- task4/server/src/main/java/ru/focusstart/model/ServerModel.java
+++ task4/server/src/main/java/ru/focusstart/model/ServerModel.java
@@ -3,7 +3,10 @@ package ru.focusstart.model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reader.PropertieReader;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -14,6 +17,8 @@ public class ServerModel {
     private int portNumber;
     ServerSocket serverSocket;
     List<Socket> clients;
+    List<BufferedReader> readers;
+
     private static final Logger log = LoggerFactory.getLogger(ServerModel.class);
 
     public static ServerModel getInstance() {
@@ -21,6 +26,7 @@ public class ServerModel {
             try {
                 instance = new ServerModel();
             } catch (IOException e) {
+                //  System.setProperty("log.name", String.valueOf(ServerModel.class.getResource("/log/server.log")));
                 log.info(e.getMessage());
             }
         }
@@ -30,31 +36,32 @@ public class ServerModel {
     private ServerModel() throws IOException {
         PropertieReader portNumberReader = new PropertieReader(ServerModel.class, "/server.properties");
         this.portNumber = portNumberReader.read("server.port");
-        this.serverSocket = new ServerSocket(portNumber);
         clients = new ArrayList<>();
+        readers = new ArrayList<>();
+        //writers =  new ArrayList<>();
     }
 
-    public void start(){
+    public void start() {
         while (true) {
             Socket clientSocket = null;
             try {
+                this.serverSocket = new ServerSocket(portNumber);
                 clientSocket = serverSocket.accept();
+                readers.add(new BufferedReader(new InputStreamReader(clientSocket.getInputStream())));
                 log.info(clientSocket.toString());
             } catch (IOException e) {
                 log.info(e.getMessage());
             }
             clients.add(clientSocket);
-            //readers.add(new BufferedReader(new InputStreamReader(clientSocket.getInputStream())));
-            //writers.add(new PrintWriter(clientSocket.getOutputStream()));
         }
     }
 
-    public void stop(){
+    public void watch() {
         try {
             serverSocket.close();
         } catch (IOException e) {
             log.info(e.getMessage());
-        }finally {
+        } finally {
             for (Socket socket : clients) {
                 try {
                     socket.close();
